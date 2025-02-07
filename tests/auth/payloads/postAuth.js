6b9eccd0fbaaa32eeff16289555090dc78a15ca2
Lucas Gimenes Koritar <lucas.koritar@outlook.com>
2025-02-07 02:23:27 -0300

    fix USERNAME env

--- tests/auth/payloads/postAuth.js
+++ tests/auth/payloads/postAuth.js
@@ -3,8 +3,8 @@ function payloadPostAuth() {
         "grant_type": "password",
         "client_id": process.env.CLIENT_ID,
         "client_secret": process.env.CLIENT_SECRET,
-        "username": process.env.USERNAME,
-        "password": process.env.PASSWORD
+        "username": process.env.USERNAME_MIDAZ,
+        "password": process.env.PASSWORD_MIDAZ
     })
 };
 
